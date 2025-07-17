c6d3ccea62d2269c7a3ffb91c1cb9865f962e256
babacar13 <mcelo6152@gmail.com>
2025-07-18 00:48:03 +0200

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+// 📁 src/App.jsx
+
+import React from 'react';
+
+export default function App() {
+  return (
+    <div style={{ textAlign: 'center', marginTop: '50px' }}>
+      <h1>Bienvenue sur AfriSmart 🚀</h1>
+      <p>Site propulsé par Vite + React</p>
+    </div>
+  );
+}
